1da45d2b54c21ffba67369442282ba12540eec63
Jamie Escalante <jamie.escalante7@gmail.com>
2021-06-04 12:50:15 -0400

    Jescalante: ADD Cambios de BSilva en custom_image_maker

--- custom_image_maker/models/sale.py
+++ custom_image_maker/models/sale.py
@@ -5,8 +5,13 @@ from odoo import models, fields, api, _
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
-    solid = fields.Boolean('SOLID')
-    srv_start_date = fields.Date('Fecha inicio de servicios')
+    solid = fields.Boolean('BEST ESTIMATE')
+    srv_start_date = fields.Date('Fecha estimada de inicio')
     srv_end_date = fields.Date('Fecha fin de servicios')
     percentage = fields.Float("Porcentaje")
     validate = fields.Boolean('Validar')
+    estado = fields.Selection(selection=[
+        ('solid', 'Solid'),
+        ('stretch', 'Stretch'),
+        ('best', 'Best Case')
+    ])
