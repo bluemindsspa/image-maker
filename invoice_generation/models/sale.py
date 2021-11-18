8314ca3433cac7191e9235994ee20e01cec51955
Boris <silvaboris@gmail.com>
2021-11-18 13:11:40 -0400

    [FIX] Correccion error en codigo

--- invoice_generation/models/sale.py
+++ invoice_generation/models/sale.py
@@ -44,7 +44,7 @@ class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
     def _prepare_invoice_line(self, **optional_values):
-        res = super(SaleOrderLine, self)._prepare_invoice_line()
+        res = super(SaleOrderLine, self)._prepare_invoice_line(**optional_values)
         if self.currency_id.name == 'UF':
             # try:
             if self.currency_id.rate > 0:
