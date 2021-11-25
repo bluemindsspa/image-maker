55a8417b445773126099f361de0d7599958d7609
Frank Quatromani <fquatromani@gmail.com>
2021-11-24 22:03:35 -0400

    fquatromani: MOD Ajuste modulo UF a CLP

--- invoice_generation/models/account_move.py
+++ invoice_generation/models/account_move.py
@@ -36,15 +36,19 @@ class AccountMove(models.Model):
                 inv.action_post()
         return True
 
-    @api.constrains("reference", "partner_id", "company_id", "move_type", "journal_document_class_id")
+    # @api.constrains("reference", "partner_id", "company_id", "move_type", "journal_document_class_id")
+    @api.constrains("reference", "partner_id", "company_id", "move_type", "l10n_latam_document_type_id")
     def _check_reference_in_invoice(self):
         for record in self:
-            if record.move_type in ["in_invoice", "in_refund"] and record.sii_document_number:
+            # if record.move_type in ["in_invoice", "in_refund"] and record.sii_document_number:
+            if record.move_type in ["in_invoice", "in_refund"] and record.l10n_latam_document_number:
                 domain = [
                     ("move_type", "=", record.move_type),
-                    ("sii_document_number", "=", record.sii_document_number),
+                    # ("sii_document_number", "=", record.sii_document_number),
+                    ("l10n_latam_document_number", "=", record.l10n_latam_document_number),
                     ("partner_id", "=", record.partner_id.id),
-                    ("journal_document_class_id.sii_document_class_id", "=", record.document_class_id.id),
+                    # ("journal_document_class_id.sii_document_class_id", "=", record.journal_document_class_id.id),
+                    ("l10n_latam_document_type_id", "=", record.l10n_latam_document_type_id.id),
                     ("company_id", "=", record.company_id.id),
                     ("id", "!=", record.id),
                     ("state", "!=", "cancel"),
@@ -54,7 +58,7 @@ class AccountMove(models.Model):
                     raise UserError(
                         u"El numero de factura debe ser unico por Proveedor.\n"
                         u"Ya existe otro documento con el numero: %s para el proveedor: %s"
-                        % (record.sii_document_number, record.partner_id.display_name)
+                        % (record.l10n_latam_document_number, record.partner_id.display_name)
                     )
 
 
