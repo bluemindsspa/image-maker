605b0c72bed0eb3a5ab2f758ef30d20510756f0d
Boris <silvaboris@gmail.com>
2022-02-24 18:45:28 -0400

    [MOD] Agregar campos cabecera Balance 8 col.

--- l10n_cl_im_reports/models/eightcolumns_report.py
+++ l10n_cl_im_reports/models/eightcolumns_report.py
@@ -12,3 +12,23 @@ class CL8ColumnsReport(models.AbstractModel):
         res = super(CL8ColumnsReport, self)._get_lines(options, line_id)
         res[-2]['name'] = 'Utilidad del Ejercicio'
         return res
+
+    @api.model
+    def _get_templates(self):
+        templates = super(CL8ColumnsReport, self)._get_templates()
+        templates['main_table_header_template'] = 'l10n_cl_im_reports.main_table_header_eightcolumns'
+        return templates
+
+    def _get_columns_name(self, options):
+        columns = [
+            {'name': _("Cuenta")},
+            {'name': _("Débito"), 'class': 'number'},
+            {'name': _("Crédito"), 'class': 'number'},
+            {'name': _("Deudor"), 'class': 'number'},
+            {'name': _("Acreedor"), 'class': 'number'},
+            {'name': _("Activo"), 'class': 'number'},
+            {'name': _("Pasivo"), 'class': 'number'},
+            {'name': _("Perdida"), 'class': 'number'},
+            {'name': _("Ganancia"), 'class': 'number'}
+        ]
+        return columns
