9eb70b0937f3db4f3a7926f9d7aa68e8df65dd59
Gabriela Paredes <gparedes@blueminds.cl>
2022-03-16 12:20:49 -0400

    gparedes: MO vista search modificada dentro de los presupuestos.

--- custom_sale_IM/__manifest__.py
+++ custom_sale_IM/__manifest__.py
@@ -5,9 +5,7 @@
     'version': '14.0.1.0.0',
     'depends': ['base','sale','hr', 'custom_image_maker'],
     'data': [
-        # 'security/ir.model.access.csv',
         'views/sale_views.xml',
-        # 'views/res_config_view_inherit.xml',
     ],
     'demo': [
         #'demo/demo.xml',
